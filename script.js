ee5480e2b52aa0e75dbac5bd75353ed259ed997f
Allyson <46270902+allysonbelo@users.noreply.github.com>
2021-08-07 15:53:09 -0300

    alteração no header, menu e sidebar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.querySelector(".menu").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show")
 )
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header")
+    header.classList.toggle("scrolling", window.scrollY > 0)
+})
