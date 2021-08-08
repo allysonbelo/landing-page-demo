7efb7e20ea1937745645804b44ffa870c7a677cf
Allyson <46270902+allysonbelo@users.noreply.github.com>
2021-08-08 14:32:06 -0300

    Sessão works finalizada

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+//aplica a classe .show no container ao clicar no botão de menu
 document.querySelector(".menu").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show")
 )
 
-window.addEventListener("scroll", function(){
+//aplica a classe .scrolling no cabeçalho ao rolar a página
+window.addEventListener("scroll", function () {
     let header = document.querySelector("header")
     header.classList.toggle("scrolling", window.scrollY > 0)
 })
+
+//pega a posição exata de um determinado elemento na página
+const p = document.querySelector("#posicao")
+const posicoes = p.getBoundingClientRect()
+console.log(posicoes)
